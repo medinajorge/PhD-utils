0ee22d533a80ad6d800aca02318536732ba6e971
medinajorge <medinahdezjorge@gmail.com>
2022-11-08 18:30:46 +0100

    setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='phdu',
-    version='1.0.0',
+    version='1.0.1',
     author="Jorge Medina Hernández",
     author_email='medinahdezjorge@gmail.com',
     packages=['phdu'],
