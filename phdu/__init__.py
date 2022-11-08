625f19ae93481663d97177b0cf10526a9a541427
medinajorge <medinahdezjorge@gmail.com>
2022-11-08 18:45:28 +0100

    setup

--- phdu/__init__.py
+++ phdu/__init__.py
@@ -1,7 +1,8 @@
 from tidypath import savedata, savefig, add_arg, modify_arg, delete_arg
 from . import fmt
 from . import storage
+from . import _helper
+from . import plots
 from . import stats
 from .stats.rtopy import resample
 from .stats.test import permutation
-from . import plots
