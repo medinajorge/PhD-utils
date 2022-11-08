ed32aac192bb92e40f11c8e38711f6a8e91999ec
medinajorge <medinahdezjorge@gmail.com>
2022-11-08 18:38:54 +0100

    setup

--- phdu/__init__.py
+++ phdu/__init__.py
@@ -1,7 +1,5 @@
 from tidypath import savedata, savefig, add_arg, modify_arg, delete_arg
 from . import fmt
-from . import plots
-from . import stats
 from . import storage
 from .stats.rtopy import resample
 from .stats.test import permutation
--- phdu/stats/plots.py
+++ phdu/stats/plots.py
@@ -9,7 +9,7 @@ except:
     pass
 
 from . import _preprocess
-from .. import plots
+from ..plots.plotly import get_figure
 
 def qqplot(x, alpha=0.3, ms=20):
         pp = sm.ProbPlot(x, fit=True)
@@ -18,7 +18,7 @@ def qqplot(x, alpha=0.3, ms=20):
         return plt.gcf()
     
 def density_kernel(*X, cov_factor=0.1, n_points=300, **kwargs):
-    fig = plots.plotly.get_figure(yaxis_title="Probability density", **kwargs)
+    fig = get_figure(yaxis_title="Probability density", **kwargs)
     for x in X:
         xs, density = _preprocess.density_kernel(x, cov_factor=cov_factor, n_points=n_points)
         
