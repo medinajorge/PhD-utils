08241632c46fce76f4dbfdcfd62aef81ad3ccf00
medinajorge <medinahdezjorge@gmail.com>
2022-11-09 09:55:45 +0100

    typo

--- phdu/plots/base.py
+++ phdu/plots/base.py
@@ -17,7 +17,7 @@ def break_str(x, separator="<br>"):
 
 def plotly_default_colors(maxlen=None):
     colors = ['#1f77b4', '#ff7f0e',  '#2ca02c',  '#d62728',  '#9467bd',  '#8c564b',  '#e377c2',  '#7f7f7f',  '#bcbd22',  '#17becf']
-    return _helper.sequence_or_stream(cs, maxlen=maxlen)
+    return _helper.sequence_or_stream(colors, maxlen=maxlen)
 
 def plotly_colors(maxlen=None):
     cs = """aliceblue, antiquewhite, aqua, aquamarine, azure,
