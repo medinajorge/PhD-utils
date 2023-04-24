89200e76bea5fbd5d15e161332582d7fa3ad3049
medinajorge <medinahdezjorge@gmail.com>
2023-04-24 15:58:07 +0200

    delete all files starting at root

--- phdu/storage.py
+++ phdu/storage.py
@@ -10,8 +10,8 @@ parent_dir =  'data'
 def move_files(keyword, folder=None, parent_dir=parent_dir, verbose=1):
     """
     Moves all files in the parent directory starting with a keyword to a folder.
-    
-    Attributes:    
+
+    Attributes:
         - keyword: keyword to look for during file matching-
         - folder: Created folder to store all files matched by the keyword
         - parend_dir: Initial directory in which the files are contained.
@@ -24,11 +24,11 @@ def move_files(keyword, folder=None, parent_dir=parent_dir, verbose=1):
             continue
         else:
             if re.findall(key, path):
-                os.rename(os.path.join(parent_dir, path), 
+                os.rename(os.path.join(parent_dir, path),
                           os.path.join(parent_dir, folder, path))
                 processed += 1
     if verbose:
-        print(f"Moved {processed} files from '{parent_dir}' to '{folder}' subdirectory.")        
+        print(f"Moved {processed} files from '{parent_dir}' to '{folder}' subdirectory.")
     return
 
 def delete_files_by_ext(parent_dir, extensions, verbose=1):
@@ -42,7 +42,7 @@ def delete_files_by_ext(parent_dir, extensions, verbose=1):
                 os.remove(os.path.join(parent_dir, path))
                 deleted += 1
     if verbose:
-        print(f"Deleted {deleted} files.")        
+        print(f"Deleted {deleted} files.")
     return
 
 def delete_stdin_files(parent_dir="nuredduna_programmes/stdin_files", verbose=1, completed_only=True, key="Done", key_search="any"):
@@ -61,7 +61,7 @@ def delete_stdin_files(parent_dir="nuredduna_programmes/stdin_files", verbose=1,
             find_key = lambda l: l.endswith(key)
         else:
             raise ValueError(f"key_search '{key_search}' not valid. Available: 'any', 'start', 'end'.")
-        
+
         deleted_programs = 0
         deleted_files = 0
         for f in os.listdir(parent_dir):
@@ -76,7 +76,7 @@ def delete_stdin_files(parent_dir="nuredduna_programmes/stdin_files", verbose=1,
                         os.remove(ff_err)
                         deleted_files += 1
         if verbose:
-            print(f"Deleted {deleted_files} files ({deleted_programs} completed programmes).")   
+            print(f"Deleted {deleted_files} files ({deleted_programs} completed programmes).")
     else:
         delete_files_by_ext(parent_dir, [".e", ".o"], verbose)
     return
@@ -91,4 +91,19 @@ def empty_trash(verbose=1):
             deleted += 1
     if verbose:
         print(f"Deleted {deleted} files.")
+    return
+
+
+def delete_files_by_key(root, keys):
+    """Delete all files in root and following subdirectories containing at least one of the keys."""
+    # tree walk starting in directory 'data':
+    root = 'data'
+    count = 0
+    for dirpath, dirnames, filenames in os.walk(root):
+        # delete files containing 'DBS' or 'control'
+        for filename in filenames:
+            if any(key in filename for key in keys):
+                os.remove(os.path.join(dirpath, filename))
+                count += 1
+    print(f"Deleted {count} files containing at least one of {keys}")
     return
