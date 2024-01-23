eaecebc3de424c7525844bbce7d4238f95ea6405
medinajorge <medinahdezjorge@gmail.com>
2024-01-23 17:01:21 +1100

    longest sequence of Trues and Falses

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='phdu',
-    version='2.2.b2',
+    version='2.2.b3',
     author="Jorge Medina Hernández",
     author_email='medinahdezjorge@gmail.com',
     packages=find_packages("."),
