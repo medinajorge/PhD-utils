3266b6a08ea4269604347d0cf15a1029611c7f7c
medinajorge <medinahdezjorge@gmail.com>
2024-07-13 17:04:28 +0100

    updated SS corr in dataframe

--- phdu/stats/corr.py
+++ phdu/stats/corr.py
@@ -3,8 +3,16 @@ Correlation metrics
 """
 import pandas as pd
 import numpy as np
+try:
+    shell = get_ipython().__class__.__name__
+    if shell == 'ZMQInteractiveShell': # script being run in Jupyter notebook
+        from tqdm.notebook import tqdm
+    elif shell == 'TerminalInteractiveShell': #script being run in iPython terminal
+        from tqdm import tqdm
+except NameError:
+    from tqdm import tqdm # Probably runing on standard python terminal. If does not work => should be replaced by tqdm(x) = identity(x)
 
-def corr_pruned(df, method='spearman', alpha=0.05):
+def corr_pruned(df, method='spearman', alpha=0.05, ns_to_nan=True):
     """
     Returns correlation between DataFrame features with pvalue < alpha.
     """
@@ -12,13 +20,19 @@ def corr_pruned(df, method='spearman', alpha=0.05):
     corr_func = getattr(ss, f"{method}r")
     c = {}
     p = {}
-    for col1 in df.columns:
+    for col1 in tqdm(df.columns):
         for col2 in df.columns:
-            corr, pval = corr_func(*(df[[col1, col2]].dropna().values.T))
-            c[(col1, col2)] = corr
-            p[(col1, col2)] = pval
+            if (col1, col2) in c or (col2, col1) in c:
+                continue
+            elif col1 == col2:
+                c[(col1, col2)] = 1.0
+                p[(col1, col2)] = 0
+            else:
+                corr, pval = corr_func(*(df[[col1, col2]].dropna().values.T))
+                c[(col1, col2)] = corr
+                p[(col1, col2)] = pval
     c = pd.Series(c).unstack()
     p = pd.Series(p).unstack()
-    c_pruned = c.copy()
-    c_pruned[p > alpha] = np.NaN
-    return c_pruned
+    if ns_to_nan:
+        c[p > alpha] = np.NaN
+    return c
