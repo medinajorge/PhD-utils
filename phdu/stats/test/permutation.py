dbaaf12fee6c836baf06f75e996f3c59b5f45b6f
medinajorge <medinahdezjorge@gmail.com>
2024-12-16 11:39:13 +0100

    compatibility

--- phdu/stats/test/permutation.py
+++ phdu/stats/test/permutation.py
@@ -21,13 +21,16 @@ def _permutation_pvalue(alternative, perm_sample, stat_0, tolerance):
                    1)
     else:
         raise ValueError("alternative not valid. Available: 'two-sided', 'less', 'greater'.")
-    
+
 def _compile_func(func_string, env_global, use_numba):
     if func_string.startswith("    "): # Unexpected indentation.
         func_string = func_string[4:].replace("\n    ", "\n")
     code_obj = compile(func_string, '<string>', 'exec')
     if isinstance(code_obj.co_consts[-1], tuple):
-        f = types.FunctionType(code_obj.co_consts[-4], env_global, argdefs=code_obj.co_consts[-1])
+        try:
+            f = types.FunctionType(code_obj.co_consts[-4], env_global, argdefs=code_obj.co_consts[-1])
+        except:
+            f = types.FunctionType(code_obj.co_consts[1], env_global, argdefs=code_obj.co_consts[-1])
     else:
         f = types.FunctionType(code_obj.co_consts[-3], env_global, argdefs=code_obj.co_consts[:-3])
     if use_numba:
@@ -37,25 +40,25 @@ def _compile_func(func_string, env_global, use_numba):
 
 def _get_permutation_test_not_paired(stat_func, use_numba=True):
     """
-    Generate a function for the permutation test using the statistic 'stat_func'. 
+    Generate a function for the permutation test using the statistic 'stat_func'.
     This statistic can or not be numba compatible. Set 'use_numba' accordingly.
     """
     def permutation_test_STAT_FUNC_NAME(X1, X2, ratio=False, R=100000, alternative="two-sided", tolerance=1.5e-8, seed=0):
         """
         Permutation test for 'STAT_FUNC_NAME'. Permutations occur among all elements of X1 and X2.
-        
+
         H0: X1 and X2 come from the same distribution F. The joint distribution would be independent on the variables: g(x, y) = f(x)f(y)
-           
+
         Attrs:
             - alternative: one of 'two-sided', 'less', 'greater'.
                            Example of calculation for the mean mu:
-                                                - greater:    H0 compatible: mu_1 / mu_2 < observed,       H1 compatible: mu_1 / mu_2  >= observed  
-                              ratio             - less:       H0 compatible: mu_1 / mu_2 > observed,       H1 compatible: mu_1 / mu_2  <= observed  
-                                                - two-sided:  H0 compatible: mu_1 / mu_2 = observed,       H1 compatible: mu_1 / mu_2  != observed 
-            
-                                                - greater:    H0 compatible: mu_1 - mu_2 < observed,       H1 compatible: mu_1 - mu_2  >= observed  
-                              not ratio         - less:       H0 compatible: mu_1 - mu_2 > observed,       H1 compatible: mu_1 - mu_2  <= observed  
-                                                - two-sided:  H0 compatible: mu_1 - mu_2 = observed,       H1 compatible: mu_1 - mu_2  != observed 
+                                                - greater:    H0 compatible: mu_1 / mu_2 < observed,       H1 compatible: mu_1 / mu_2  >= observed
+                              ratio             - less:       H0 compatible: mu_1 / mu_2 > observed,       H1 compatible: mu_1 / mu_2  <= observed
+                                                - two-sided:  H0 compatible: mu_1 / mu_2 = observed,       H1 compatible: mu_1 / mu_2  != observed
+
+                                                - greater:    H0 compatible: mu_1 - mu_2 < observed,       H1 compatible: mu_1 - mu_2  >= observed
+                              not ratio         - less:       H0 compatible: mu_1 - mu_2 > observed,       H1 compatible: mu_1 - mu_2  <= observed
+                                                - two-sided:  H0 compatible: mu_1 - mu_2 = observed,       H1 compatible: mu_1 - mu_2  != observed
             - R:           number of resamples (permutations).
             - tolerance:   tolerance for numerical stability.
             - seed: seed   for random number generation.
@@ -70,40 +73,40 @@ def _get_permutation_test_not_paired(stat_func, use_numba=True):
                 return stat_func(X[:n1]) - stat_func(X[n1:])
         X = np.hstack((X1, X2))
         stat_0 = aux(X, n1)
-           
+
         perm_sample = np.empty((R)) # permutation distribution
         np.random.seed(seed)
-        for i in range(R):       
+        for i in range(R):
             np.random.shuffle(X)
             perm_sample[i] = aux(X, n1)
         return _permutation_pvalue(alternative, perm_sample, stat_0, tolerance)
-    
+
     func_source_edit = inspect.getsource(permutation_test_STAT_FUNC_NAME).replace("STAT_FUNC_NAME", stat_func.__name__)
     env = globals()
     env.update(locals())
     return _compile_func(func_source_edit, env, use_numba)
-    
+
 def _get_permutation_test_paired(stat_func, use_numba=True):
     """
-    Generate a function for the paired permutation test using the statistic 'stat_func'. 
+    Generate a function for the paired permutation test using the statistic 'stat_func'.
     This statistic can or not be numba compatible. Set 'use_numba' accordingly.
     """
     def permutation_test_paired_STAT_FUNC_NAME(X1, X2, ratio=False, R=100000, alternative="two-sided", tolerance=1.5e-8, seed=0):
         """
         Paired permutation test for 'STAT_FUNC_NAME'. Permutations occur only between each pair:   (x1, x2)  <->  (x2, x1).
-        
+
         H0: (X1, X2) come from a symmetric joint distribution: p(x, y) = p(y, x).
-        
+
         Attrs:
             - alternative: one of 'two-sided', 'less', 'greater'.
                            Example of calculation for the mean mu:
-                                                - greater:    H0 compatible: mu_1 / mu_2 < observed,       H1 compatible: mu_1 / mu_2  >= observed  
-                              ratio             - less:       H0 compatible: mu_1 / mu_2 > observed,       H1 compatible: mu_1 / mu_2  <= observed  
-                                                - two-sided:  H0 compatible: mu_1 / mu_2 = observed,       H1 compatible: mu_1 / mu_2  != observed 
-            
-                                                - greater:    H0 compatible: mu_1 - mu_2 < observed,       H1 compatible: mu_1 - mu_2  >= observed  
-                              not ratio         - less:       H0 compatible: mu_1 - mu_2 > observed,       H1 compatible: mu_1 - mu_2  <= observed  
-                                                - two-sided:  H0 compatible: mu_1 - mu_2 = observed,       H1 compatible: mu_1 - mu_2  != observed 
+                                                - greater:    H0 compatible: mu_1 / mu_2 < observed,       H1 compatible: mu_1 / mu_2  >= observed
+                              ratio             - less:       H0 compatible: mu_1 / mu_2 > observed,       H1 compatible: mu_1 / mu_2  <= observed
+                                                - two-sided:  H0 compatible: mu_1 / mu_2 = observed,       H1 compatible: mu_1 / mu_2  != observed
+
+                                                - greater:    H0 compatible: mu_1 - mu_2 < observed,       H1 compatible: mu_1 - mu_2  >= observed
+                              not ratio         - less:       H0 compatible: mu_1 - mu_2 > observed,       H1 compatible: mu_1 - mu_2  <= observed
+                                                - two-sided:  H0 compatible: mu_1 - mu_2 = observed,       H1 compatible: mu_1 - mu_2  != observed
             - R:           number of resamples (permutations).
             - tolerance:   tolerance for numerical stability.
             - seed: seed   for random number generation.
@@ -115,11 +118,11 @@ def _get_permutation_test_paired(stat_func, use_numba=True):
         else:
             def aux(X_paired):
                 return stat_func(X_paired[0]) - stat_func(X_paired[1])
-            
+
         n = X1.size
         X_paired = np.vstack((X1, X2))
         stat_0 = aux(X_paired)
-        
+
         perm_sample = np.empty((R))
         np.random.seed(seed)
         for i in range(R):
@@ -129,35 +132,35 @@ def _get_permutation_test_paired(stat_func, use_numba=True):
             X_perm = np.hstack((X_shifted, X_still))
             perm_sample[i] = aux(X_perm)
         return _permutation_pvalue(alternative, perm_sample, stat_0, tolerance)
-    
+
     func_source_edit = inspect.getsource(permutation_test_paired_STAT_FUNC_NAME).replace("STAT_FUNC_NAME", stat_func.__name__)
     env = globals()
     env.update(locals())
     return _compile_func(func_source_edit, env, use_numba)
-    
+
 def _get_permutation_test_block(stat_func, use_numba=True):
     """
     Generate a function for the block permutation test using the statistic 'stat_func'.
     This statistic can or not be numba compatible. Set 'use_numba' accordingly.
-    Permutations occur only between blocks: X1(block i) <-> X2(block i). 
+    Permutations occur only between blocks: X1(block i) <-> X2(block i).
     """
     def permutation_test_block_STAT_FUNC_NAME(X, Y, ratio=False, R=100000, alternative="two-sided", tolerance=1.5e-8, seed=0):
         """
-        Block permutation test for 'STAT_FUNC_NAME'. Permutations occur only between blocks: X1(block i) <-> X2(block i). 
-        
+        Block permutation test for 'STAT_FUNC_NAME'. Permutations occur only between blocks: X1(block i) <-> X2(block i).
+
         H0: (X, Y) come from a joint distribution symmetric between each block:   p(x, y) = p(y, x)  <=> y, x belong to the same block.
-        
+
         Attrs:
-            - X, Y:        ragged arrays or tuples. Each element is an array containing the results for a block. 
+            - X, Y:        ragged arrays or tuples. Each element is an array containing the results for a block.
             - alternative: one of 'two-sided', 'less', 'greater'.
                            Example of calculation for the mean mu:
-                                                - greater:    H0 compatible: mu_1 / mu_2 < observed,       H1 compatible: mu_1 / mu_2  >= observed  
-                              ratio             - less:       H0 compatible: mu_1 / mu_2 > observed,       H1 compatible: mu_1 / mu_2  <= observed  
-                                                - two-sided:  H0 compatible: mu_1 / mu_2 = observed,       H1 compatible: mu_1 / mu_2  != observed 
-            
-                                                - greater:    H0 compatible: mu_1 - mu_2 < observed,       H1 compatible: mu_1 - mu_2  >= observed  
-                              not ratio         - less:       H0 compatible: mu_1 - mu_2 > observed,       H1 compatible: mu_1 - mu_2  <= observed  
-                                                - two-sided:  H0 compatible: mu_1 - mu_2 = observed,       H1 compatible: mu_1 - mu_2  != observed 
+                                                - greater:    H0 compatible: mu_1 / mu_2 < observed,       H1 compatible: mu_1 / mu_2  >= observed
+                              ratio             - less:       H0 compatible: mu_1 / mu_2 > observed,       H1 compatible: mu_1 / mu_2  <= observed
+                                                - two-sided:  H0 compatible: mu_1 / mu_2 = observed,       H1 compatible: mu_1 / mu_2  != observed
+
+                                                - greater:    H0 compatible: mu_1 - mu_2 < observed,       H1 compatible: mu_1 - mu_2  >= observed
+                              not ratio         - less:       H0 compatible: mu_1 - mu_2 > observed,       H1 compatible: mu_1 - mu_2  <= observed
+                                                - two-sided:  H0 compatible: mu_1 - mu_2 = observed,       H1 compatible: mu_1 - mu_2  != observed
             - R:           number of resamples (permutations).
             - tolerance:   tolerance for numerical stability.
             - seed: seed   for random number generation.
@@ -169,11 +172,11 @@ def _get_permutation_test_block(stat_func, use_numba=True):
         else:
             def aux(X1, X2):
                 return stat_func(X1) - stat_func(X2)
-            
+
         def stack(arr_list):
             return np.array([a for arr in arr_list for a in arr])
-            
-        stat_0 = aux(stack(X), stack(Y))    
+
+        stat_0 = aux(stack(X), stack(Y))
         perm_sample = np.empty((R))
         np.random.seed(seed)
         for i in range(R):
@@ -186,22 +189,22 @@ def _get_permutation_test_block(stat_func, use_numba=True):
                 Yi.append(z[xi.size:])
             perm_sample[i] = aux(stack(Xi), stack(Yi))
         return _permutation_pvalue(alternative, perm_sample, stat_0, tolerance)
-    
+
     func_source_edit = inspect.getsource(permutation_test_block_STAT_FUNC_NAME).replace("STAT_FUNC_NAME", stat_func.__name__)
     env = globals()
     env.update(locals())
     return _compile_func(func_source_edit, env, use_numba)
-    
+
 def get_permutation_test(stat_func, pairing=None, use_numba=True):
     """
     Generate a function for the permutation test using the statistic 'stat_func'.
     This statistic can or not be numba compatible. Set 'use_numba' accordingly.
-    
+
     pairing:
         - None:     Permutations occur among all elements of X1 and X2.
         - 'paired': Permutations occur only between each pair:   (x1, x2)  <->  (x2, x1).   (x1, x2) are elements of (X1, X2).
         - 'block':  Permutations occur only between blocks:      (x1_b, x2_b)  <->  (x2_b, x1_b).   x1_b in X1(block b), x2_b in X2(block b).
-    
+
     Returns: function that computes the permutation test using the statistic 'stat_func'.
     """
     if pairing is None:
@@ -222,7 +225,7 @@ permutation_test_block_mean = get_permutation_test(np.median, pairing='block')
 
 
 ## Permutation test for mean/median of the difference or ratio
- 
+
 @njit
 def permutation_test_paired_diffmean(X1, X2, R=int(1e5)-1, alternative="two-sided", tolerance=1.5e-8, seed=0):
     """
@@ -237,23 +240,23 @@ def permutation_test_paired_diffmean(X1, X2, R=int(1e5)-1, alternative="two-side
     dX = X1 - X2
     n = dX.size
     stat_0 = dX.mean()
-    
+
     perm_sample = np.empty((R))
     np.random.seed(seed)
-    for i in range(R):        
+    for i in range(R):
         shuffle = np.random.randint(0, 2, size=n) == 1
         dX_perm = dX.copy()
         dX_perm[shuffle] *= -1
         perm_sample[i] = (dX_perm).mean()
-    
+
     return _permutation_pvalue(alternative, perm_sample, stat_0, tolerance)
-        
+
 @njit
 def permutation_test_paired_diffmedian(X1, X2, R=int(1e5)-1, alternative="two-sided", tolerance=1.5e-8, seed=0):
     """
     Paired permutation test optimized for the median of the differences. about 2x faster than the default _permutation_test_2sample_paired with stat_func = np.mean.
     NOTE: Does not coincide with the test for the differences in medians.
-    
+
     Attrs:
             alternative:  - greater:    H0: me(X1-X2) < observed,       H1: me(X1-X2) >= observed
                           - less:       H0: me(X1-X2) > observed,       H1: me(X1-X2) <= observed
@@ -263,37 +266,37 @@ def permutation_test_paired_diffmedian(X1, X2, R=int(1e5)-1, alternative="two-si
     dX = X1 - X2
     n = dX.size
     stat_0 = np.median(dX)
-    
+
     perm_sample = np.empty((R))
     np.random.seed(seed)
-    for i in range(R):        
+    for i in range(R):
         shuffle = np.random.randint(0, 2, size=n) == 1
         dX_perm = dX.copy()
         dX_perm[shuffle] *= -1
         perm_sample[i] = np.median(dX_perm)
-    
+
     return _permutation_pvalue(alternative, perm_sample, stat_0, tolerance)
 
 @njit
 def permutation_test_paired_ratio_median(X1, X2, R=int(1e5)-1, alternative="two-sided", tolerance=1.5e-8, seed=0):
     """
     Paired permutation test for the median ratio.
-    
+
     Attrs:
-            alternative: 
-                                           - greater:    H0: me_1 / me_2 < observed,       H1: me_1 / me_2  >= observed  
-                                           - less:       H0: me_1 / me_2 > observed,       H1: me_1 / me_2  <= observed  
-                                           - two-sided:  H0: me_1 / me_2 = observed,       H1: me_1 / me_2  != observed 
-            
+            alternative:
+                                           - greater:    H0: me_1 / me_2 < observed,       H1: me_1 / me_2  >= observed
+                                           - less:       H0: me_1 / me_2 > observed,       H1: me_1 / me_2  <= observed
+                                           - two-sided:  H0: me_1 / me_2 = observed,       H1: me_1 / me_2  != observed
+
     Returns: p-value
     """
     def aux(X_paired):
             return np.median(X_paired[0] / X_paired[1])
-        
+
     n = X1.size
     X_paired = np.vstack((X1, X2))
     stat_0 = aux(X_paired)
-    
+
     perm_sample = np.empty((R))
     np.random.seed(seed)
     for i in range(R):
@@ -302,29 +305,29 @@ def permutation_test_paired_ratio_median(X1, X2, R=int(1e5)-1, alternative="two-
         X_still = X_paired[:, ~shuffle]
         X_perm = np.hstack((X_shifted, X_still))
         perm_sample[i] = aux(X_perm)
-    
+
     return _permutation_pvalue(alternative, perm_sample, stat_0, tolerance)
-    
+
 @njit
 def permutation_test_paired_ratio_mean(X1, X2, R=int(1e5)-1, alternative="two-sided", tolerance=1.5e-8, seed=0):
     """
     Paired permutation test for the median ratio.
-    
+
     Attrs:
-            alternative: 
-                                           - greater:    H0: me_1 / me_2 < observed,       H1: me_1 / me_2  >= observed  
-                                           - less:       H0: me_1 / me_2 > observed,       H1: me_1 / me_2  <= observed  
-                                           - two-sided:  H0: me_1 / me_2 = observed,       H1: me_1 / me_2  != observed 
-            
+            alternative:
+                                           - greater:    H0: me_1 / me_2 < observed,       H1: me_1 / me_2  >= observed
+                                           - less:       H0: me_1 / me_2 > observed,       H1: me_1 / me_2  <= observed
+                                           - two-sided:  H0: me_1 / me_2 = observed,       H1: me_1 / me_2  != observed
+
     Returns: p-value
     """
     def aux(X_paired):
             return np.mean(X_paired[0] / X_paired[1])
-        
+
     n = X1.size
     X_paired = np.vstack((X1, X2))
     stat_0 = aux(X_paired)
-    
+
     perm_sample = np.empty((R))
     np.random.seed(seed)
     for i in range(R):
@@ -333,5 +336,5 @@ def permutation_test_paired_ratio_mean(X1, X2, R=int(1e5)-1, alternative="two-si
         X_still = X_paired[:, ~shuffle]
         X_perm = np.hstack((X_shifted, X_still))
         perm_sample[i] = aux(X_perm)
-    
+
     return _permutation_pvalue(alternative, perm_sample, stat_0, tolerance)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='phdu',
-    version='2.8.5',
+    version='2.8.6',
     author="Jorge Medina Hernández",
     author_email='medinahdezjorge@gmail.com',
     packages=find_packages("."),
